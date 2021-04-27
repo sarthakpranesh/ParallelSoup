9e33edd3b8b1b461312ce50a1e475fb8f3e93526
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-04-27 17:19:02 +0530

    Cleaning the test example for ParallelSoup
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- tests/imdbTest.py
+++ tests/imdbTest.py
@@ -1,14 +1,7 @@
-import json
-  
-# Opening JSON file
-f = open('test.json')
-data = json.load(f)
-f.close()
-
 from parallelsoup import ParallelSoup
 
 urls = []
-for i in range(0, 100):
+for i in range(0, 10):
     urls.append("https://www.imdb.com/search/title/?countries=in&adult=include&start=" + str(50*i + 1) + "&ref_=adv_nxt")
 
 def extractor(soup):
